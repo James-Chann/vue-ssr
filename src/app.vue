8c5604adc161bcb9bf665e742c56f5aa6efb54c8
James-Chann <jameschan51@163.com>
2018-10-26 22:20:22 +0800

    基本配置webpack.config.js第一节

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello world'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: red;
+  }
+</style>
